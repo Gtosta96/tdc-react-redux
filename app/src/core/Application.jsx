5bf772111ae9951a38f0b848433b77880589150f
= <armandofreire.it@gmail.com>
2017-04-23 18:05:15 -0300

    Adiciona UI e removeCounter

--- app/src/core/Application.jsx
+++ app/src/core/Application.jsx
@@ -3,17 +3,23 @@ import React from 'react';
 
 const Application = (props) => {
 	return (
-		<div>
-			<span>TDC - React Redux!</span>
-			<div>{ props.counter }</div>
-			<button onClick={ () => props.addCounter() }>Somar</button>
+		<div className="container">
+			<h1 className="title">
+				<img width="50" src="../../tdc_logo.png" /> TDC Floripa - React + Redux
+			</h1>
+			<div className="text-center">
+				<h1 className="counter">{ props.counter }</h1>
+				<button className="btn tdc-button btn-link" onClick={ () => props.addCounter() }>➕</button>
+				<button className="btn tdc-button btn-link" onClick={ () => props.removeCounter() }>➖</button>
+			</div>			
 		</div>
 	);
 };
 
 Application.propTypes = {
 	counter: React.PropTypes.number.isRequired,
-	addCounter: React.PropTypes.func.isRequired
+	addCounter: React.PropTypes.func.isRequired,
+	removeCounter: React.PropTypes.func.isRequired
 };
 
 export default Application;
